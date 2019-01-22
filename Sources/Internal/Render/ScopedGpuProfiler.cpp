271695a1cc88e68cc631910bf7a57fd0a86dd94a
Alex Vorontsov <nameless323@gmail.com>
2019-01-22 07:39:16 -0500

    Change year.

--- Sources/Internal/Render/ScopedGpuProfiler.cpp
+++ Sources/Internal/Render/ScopedGpuProfiler.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (C) Aleksandr Vorontcov 2018.
+// Copyright (C) Aleksandr Vorontcov 2019.
 // Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
 //
 
