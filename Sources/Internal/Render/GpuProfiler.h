1bd25ad8c52dae5f0fa2c0c0f2eb6600c3b2449b
Alex Vorontсov <nameless323@gmail.com>
2020-04-06 21:03:36 -0400

    Some part of the renderer

--- Sources/Internal/Render/GpuProfiler.h
+++ Sources/Internal/Render/GpuProfiler.h
@@ -1,8 +1,3 @@
-//
-// Copyright (C) Aleksandr Vorontcov 2019.
-// Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
-//
-
 #pragma once
 
 #include <string>
