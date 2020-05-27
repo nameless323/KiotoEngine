1540693cd14c05cd65d6789b5bb8e7bb1f561d3c
Alex Vorontсov <nameless323@gmail.com>
2020-05-26 20:58:25 -0400

    Build full context

---
+++ ShaderInputsParser/ShaderInputsParser/Source/Sampler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShaderInputsParserApp.Source
+{
+    class Sampler : IBindable
+    {
+        public Sampler(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+        public BindpointDesc Bindpoint { get; set; }
+        public List<Annotation> Annotations { get; set; }
+    }
+}
--- ShaderInputsParser/ShaderInputsParser/Source/ShaderInputsVisitor.cs
+++ ShaderInputsParser/ShaderInputsParser/Source/ShaderInputsVisitor.cs
@@ -11,6 +11,8 @@ namespace ShaderInputsParserApp.Source
         public List<Structure> Structures { get; set; } = new List<Structure>();
         public List<string> Includes { get; set; } = new List<string>();
         public List<ConstantBuffer> ConstantBuffers { get; set; } = new List<ConstantBuffer>();
+        public List<Texture> Textures { get; set; } = new List<Texture>();
+        public List<Sampler> Samplers { get; set; } = new List<Sampler>();
 
     }
 
@@ -46,7 +48,22 @@ namespace ShaderInputsParserApp.Source
         public override string VisitTex2d(ShaderInputsParser.Tex2dContext context)
         {
             string name = context.NAME().GetText();
+            Texture t = new Texture(name);
+            OutputContext.Textures.Add(t);
             return base.VisitTex2d(context);
+        }
+        public override string VisitInclude(ShaderInputsParser.IncludeContext context)
+        {
+            string path = context.FILEPATH().GetText();
+            OutputContext.Includes.Add(path);
+            return base.VisitInclude(context);
+        }
+        public override string VisitSampler(ShaderInputsParser.SamplerContext context)
+        {
+            string name = context.NAME().GetText();
+            Sampler s = new Sampler(name);
+            OutputContext.Samplers.Add(s);
+            return base.VisitSampler(context);
         }
         public ShaderOutputContext OutputContext { get; private set; } = new ShaderOutputContext();
     }
