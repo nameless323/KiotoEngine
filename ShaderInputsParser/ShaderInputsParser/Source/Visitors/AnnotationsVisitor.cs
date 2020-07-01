3d19f4b681681c3aac573e8c861e7ea5bd92eeb6
Alex Vorontсov <nameless323@gmail.com>
2020-06-30 20:43:37 -0400

    Fixed annotations

--- ShaderInputsParser/ShaderInputsParser/Source/Visitors/AnnotationsVisitor.cs
+++ ShaderInputsParser/ShaderInputsParser/Source/Visitors/AnnotationsVisitor.cs
@@ -15,13 +15,36 @@ namespace ShaderInputsParserApp.Source
         public override string VisitAnnotationMember(ShaderInputsParser.AnnotationMemberContext context)
         {
             string name = context.NAME().ToString();
-            string value = context.VALUE()?.ToString() ?? "";
+            AnnotationValueVisitor valueVisitor = new AnnotationValueVisitor();
+            valueVisitor.Visit(context);
             Annotation annotation;
             annotation.Name = name;
-            annotation.Value = value;
+            annotation.Value = valueVisitor.Value;
             Annotations.Add(annotation);
             return base.VisitAnnotationMember(context);
         }
         public List<Annotation> Annotations = new List<Annotation>();
     }
+
+    class AnnotationValueVisitor : ShaderInputsParserBaseVisitor<string>
+    {
+        public override string VisitAnnotationValue(ShaderInputsParser.AnnotationValueContext context)
+        {
+            if (context.NAME() != null)
+            {
+                Value = context.NAME().ToString();
+                return base.VisitAnnotationValue(context);
+            }
+            if (context.NUMBER() != null)
+            {
+                foreach (var n in context.NUMBER())
+                    Value += n.ToString();
+                return base.VisitAnnotationValue(context);
+            }
+            Value = "";
+            return base.VisitAnnotationValue(context);
+        }
+        public string Value { get; set; }
+    }
+
 }
