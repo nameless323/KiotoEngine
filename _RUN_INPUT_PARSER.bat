953afc2d7781026020c855250c784504daad01eb
Alex Vorontсov <nameless323@gmail.com>
2022-02-09 21:16:06 -0500

    Updated sinp framework

--- _RUN_INPUT_PARSER.bat
+++ _RUN_INPUT_PARSER.bat
@@ -1,4 +1,4 @@
 echo off
 set ProjectDir=%1
 echo Running shader parser with command line inDir: %ProjectDir%Assets\Shaders\sInp hlslOutDir: %ProjectDir%Assets\autogen cppOutDir: %ProjectDir%Sources\Internal\Render\Shaders\autogen templatesDir: %ProjectDir%ShaderInputsParser\ShaderInputsParser\Templates
-%ProjectDir%ShaderInputsParser\ShaderInputsParser\bin\Debug\netcoreapp3.1\ShaderInputsParser.exe inDir: %ProjectDir%Assets\Shaders\sInp hlslOutDir: %ProjectDir%Assets\Shaders\autogen cppOutDir: %ProjectDir%Sources\Internal\Render\Shaders\autogen templatesDir: %ProjectDir%ShaderInputsParser\ShaderInputsParser\Templates shadersDir: %ProjectDir%Assets\Shaders forceRegenerate
+%ProjectDir%ShaderInputsParser\ShaderInputsParser\bin\Debug\net6.0\ShaderInputsParser.exe inDir: %ProjectDir%Assets\Shaders\sInp hlslOutDir: %ProjectDir%Assets\Shaders\autogen cppOutDir: %ProjectDir%Sources\Internal\Render\Shaders\autogen templatesDir: %ProjectDir%ShaderInputsParser\ShaderInputsParser\Templates shadersDir: %ProjectDir%Assets\Shaders forceRegenerate
