f8ba6c1f2f0c16a6c53b4399cd20d4094a8315d1
Alex Vorontсov <nameless323@gmail.com>
2022-02-16 20:19:20 -0500

    And again fixed this annoying "no target architecture"

--- Sources/Internal/Core/Scene.cpp
+++ Sources/Internal/Core/Scene.cpp
@@ -1,5 +1,11 @@
 #include "Core/Scene.h"
 
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers.
+#endif
+
+#include <Windows.h>
 #include <debugapi.h>
 
 #include "CoreHelpers.h"
